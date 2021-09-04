14fae54b7b1355c3e7fd797da9a2ee1045ee06af
SimplyyJoji <59666562+SimplyyJoji@users.noreply.github.com>
2021-09-04 16:13:48 -0700

    Created createLeague page and added to EventController

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -73,6 +73,34 @@ namespace CSharpExam.Controllers
         db.SaveChanges();
         return RedirectToAction("Index", "Home");
       }
+
+      [HttpGet("/league/new")]
+        public IActionResult CreateLeague()
+        {
+          return View();
+        }
+
+        [HttpPost("/league/create")]
+        public IActionResult Create(League newLeague)
+        {
+
+            // Every time a form is submitted, check the validations.
+            if (ModelState.IsValid == false)
+            {
+                // Go back to the form so error messages are displayed.
+                return View("CreateLeague");
+            }
+
+
+            // If any above custom errors were added, ModelState would now be invalid.
+
+            // HttpContext.Session.SetString("NameOne", newWedding.NameOne);
+            // newLeague.UserId = (int)uid;
+            db.Leagues.Add(newLeague);
+            db.SaveChanges();
+
+            return RedirectToAction("Dashboard");
+        }
     }
 }
 
